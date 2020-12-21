fca8f690c61772ab58b1c87b37ec90fa08dc3e6e
qsmobi <34172784+qsmobi@users.noreply.github.com>
2020-12-21 21:26:14 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sprawy.Startup))]
+namespace Sprawy
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
